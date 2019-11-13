074f4c2374ce51990fc4d49c9829eb54acaafc4b
Yashkindzhus <57439255+Yashkindzhus@users.noreply.github.com>
2019-11-13 22:09:18 +0200

    Second homework

---
+++ PopUpsApp/PopUpsApp/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PopUpsApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+            MainPage.DisplayAlert("Start App", "OnStart()", "OK");
+        }
+
+        protected override void OnSleep()
+        {
+            MainPage.DisplayAlert("Sleep App", "OnSleep()", "OK");
+        }
+
+        protected override void OnResume()
+        {
+            MainPage.DisplayAlert("Resume App", "OnResume()", "OK");
+        }
+
+  
+    }
+}
---
+++ PopUpsApp/PopUpsApp/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PopUpsApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Device.StartTimer(TimeSpan.FromSeconds(30.0), ThirtySecondsLeft);
+        }
+
+        public bool ThirtySecondsLeft()
+        {
+            PopUp();
+            return true;
+        }
+
+        public async void PopUp()
+        {
+            await DisplayAlert("Pop Up", DateTime.Now.ToString("T"), "OK");    
+        }
+
+        public async void OnSecondPageButton_Click(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SecondPage());
+        }
+    }
+}
